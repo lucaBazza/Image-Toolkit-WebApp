78389f016c51c926d01fcfa37c87ff0dcdc98bf6
lucaBazza <luca.bazzanella94@gmail.com>
2022-06-01 10:00:40 +0200

    Componente modale sopra

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h1>{{ title }}</h1>
+  <img class="headerImg" src="./assets/DSC09538-ProPs.webp"/>
+  <input type="text" ref="name">
+  <br>
+  <button @click="handleClick">click me</button>
+  <Modal />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+
+import Modal from './components/Modal.vue'
+//import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld,
+  components: { Modal },
+  data(){
+    return{
+      title: "My first image toolkit app"
+    }
   },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name)
+      let refName:any = this.$refs.name
+      refName.classList.add('active')
+      refName.focus();
+    }
+  }
+  //components: {
+  //    HelloWorld,
+  //},
 });
 </script>
 
 <style>
+/** questo è globale */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,5 +44,23 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10ox;
+  padding: 1rem;
+  color: white; 
+  background: 10rem rgba(0,0,0,.4);
+  border-radius: .5rem;
+}
+.headerImg{ 
+  z-index: -1;
+  position: fixed;
+  left:0;
+  top: 0;
+  height: 30vh;
+  width: 100%;
+  object-fit: cover;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,56 @@
+<template>
+    <div v-if="isShowed" class="backdrop">
+        <div class="modal">
+            <button class="altoDxBtn" @click="closeModal">❌</button>
+            <p>modal content</p>
+        </div>
+    </div>
+    <div v-else>
+        <button class="altoDxBtn" @click="closeModal">ℹ️</button>
+    </div>
+</template>
+<script>
+  export default {
+    data(){
+        return{
+            isShowed: false
+        }
+    },
+    methods: {
+      closeModal () {
+        this.isShowed = ! this.isShowed;
+      }
+    }
+  }
+</script>
+<style>
+.backdrop{
+    z-index:2;
+    top: 0;
+    left: 0;
+    position: fixed;
+    background: rgba(0,0,0,.5);
+    width: 100vw;
+    height: 100%;
+}
+.modal{
+    width: 200px;
+    padding: 20px;
+    margin: 40vh auto;
+    background: white;
+    border-radius: 1rem;
+    overflow: visible;
+}
+.altoDxBtn{
+    position: absolute;
+    top: 0;
+    right: 0;
+    float: right;
+    text-align: top;
+    font-size: 2rem;
+    background: transparent;
+    border: none;
+}
+
+.altoDxBtn:hover{ cursor: pointer }
+</style>
