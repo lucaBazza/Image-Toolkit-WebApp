2ddd46966d0c22d0e7e5358fafda9b05cc37ff4c
lucaBazza <luca.bazzanella94@gmail.com>
2022-07-07 17:26:51 +0200

    Base exif type + getter

---
+++ src/types/Exif.ts
@@ -0,0 +1,40 @@
+export default interface Exif {
+    Make: string
+    Model: string
+    Orientation: number | string
+	XResolution: number
+	YResolution: number
+	ResolutionUnit: number
+	DateTime: string
+	YCbCrPositioning: number
+	ExifIFDPointer: number
+	ExposureTime: number
+	FNumber: number | string
+	ExifVersion: string
+	DateTimeOriginal: string
+	DateTimeDigitized: string
+	ComponentsConfiguration: string
+	CompressedBitsPerPixel: number
+	ShutterSpeedValue: number
+	ApertureValue: number
+	ExposureBias: number
+	MaxApertureValue: number
+	MeteringMode: string
+	Flash: string
+	FocalLength: number
+	FlashpixVersion: string
+	ColorSpace: number | string
+	PixelXDimension: number
+	PixelYDimension: number
+	InteroperabilityIFDPointer: number
+	FocalPlaneXResolution: number
+	FocalPlaneYResolution: number
+	FocalPlaneResolutionUnit: number
+	SensingMethod: string
+	FileSource: string
+	CustomRendered: string
+	ExposureMode: number
+	WhiteBalance: string
+	DigitalZoomRation: number
+	SceneCaptureType: string
+}
---
+++ src/utilities/getExif.ts
@@ -0,0 +1,138 @@
+/**
+ *      npm install exif-js
+*/
+
+import EXIF from 'exif-js'
+import Exif from '@/types/Exif'
+
+export default async function getExif(imgFile) : Promise<Exif>{
+    console.log('getExif() ', imgFile.name)
+    let out
+
+    EXIF.getData(imgFile, ()=>{
+            //console.log('\n\ncallback zone!\n\n')
+            var allMetaData = EXIF.getAllTags(imgFile)
+            delete allMetaData['MakerNote']
+            delete allMetaData['UserComment']
+            delete allMetaData['thumbnail']
+            out = allMetaData as Exif // exifHelper(allMetaData)//JSON.stringify(allMetaData, null, "\t")
+            //console.log('getExif callback() ',out)
+        }
+    )
+    
+    // console.log('Wait callback finish to process...')
+    while( ! out )
+        await new Promise( res=> setTimeout(res,0) )
+    // console.log('Callback has setted exif datas! 🤗 \t ', typeof out ,'\n', out/* EXIF.getAllTags(imgFile) */ )
+
+    return out
+}
+
+
+
+
+
+
+/* 
+export default async function getExif(imgFile){
+    console.log('getExif() ',imgFile.name)
+    
+    let out
+
+    // Base implementation
+    EXIF.getData(imgFile, ()=>{
+            //console.log('\n\ncallback zone!\n\n')
+            var allMetaData = EXIF.getAllTags(imgFile)
+            delete allMetaData['MakerNote']
+            delete allMetaData['UserComment']
+            delete allMetaData['thumbnail']
+            out = JSON.stringify(allMetaData, null, "\t")
+            //console.log('getExif callback() ',out)
+        }
+    )
+    
+    console.log('Wait callback finish to process...')
+    while( ! out )
+        await new Promise(res=> setTimeout(res,0))
+    console.log('Callback has setted exif datas! 🤗 \n ', EXIF.getAllTags(imgFile) )
+
+
+    return out
+} 
+*/
+
+
+
+
+/*     
+    console.log('imgFile.meta: ', a)
+    var allMetaData = EXIF.getAllTags(imgFile)
+    delete allMetaData['MakerNote']
+    delete allMetaData['UserComment']
+    delete allMetaData['thumbnail'] 
+    return  allMetaData 
+*/
+
+
+/* function convertExitTags(imgFile){
+    console.log('\n\ncallback zone!\n\n')
+    var allMetaData = EXIF.getAllTags(imgFile)
+    delete allMetaData['MakerNote']
+    delete allMetaData['UserComment']
+    delete allMetaData['thumbnail']
+    let out = JSON.stringify(allMetaData, null, "\t")
+    console.log('getExif callback() ',out)
+}
+ */
+
+
+
+/* 
+    let data = files[0] // getImageStream(files[0])
+    console.log('exif parser factiry() \t data: ',data,files)
+    const parser = ExifParserFactory.create(data)
+    parser.enableReturnTags(false)
+    parser.enablePointers(true)
+    const resData = parser.parse()
+    
+    console.log('parser: ',parser)
+    console.log('resData: ',resData)
+    console.log('resData-tags: ',resData.tags)
+ */
+
+
+
+
+    /*     
+    await EXIF.getData(imgFile, ()=>{
+            
+        console.log('\n\n callback zone! \n\n');
+           
+        (async() => {
+            console.log('stat async...')
+            while( ! EXIF.getAllTags(imgFile) )
+                await new Promise(res=> setTimeout(res,0))
+            console.log('var is defined! ☺ \n ',
+                            JSON.stringify( EXIF.getAllTags(imgFile), null, "\t").substring(0,100))
+        })()
+
+        var allMetaData = EXIF.getAllTags(imgFile)
+        delete allMetaData['MakerNote']
+        delete allMetaData['UserComment']
+        delete allMetaData['thumbnail']
+        out = JSON.stringify(allMetaData, null, "\t")
+        console.log('getExif callback() ',out.substring(0,100))
+
+    })
+    console.log('function finished: ', out)
+ */
+
+
+/* 
+    console.log('stat async...')
+    while( ! EXIF.getAllTags(imgFile) )
+        await new Promise(res=> setTimeout(res,10))
+    console.log('var is defined! ☺ \n ', EXIF.getAllTags(imgFile) )
+                    //JSON.stringify( EXIF.getAllTags(imgFile), null, "\t").substring(0,100))
+    out = JSON.stringify( EXIF.getAllTags(imgFile), null, "\t")
+ */
